c70bfeb6d305235222062c1dd628b034946a632a
nyg0o0 <yaya56790@gmail.com>
2016-05-22 06:17:47 +0800

    track line

--- BallPane.java
+++ BallPane.java
@@ -2,22 +2,26 @@ import javafx.animation.*;
 import javafx.beans.property.DoubleProperty;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.*;
 import javafx.util.*;
 import java.util.Random;
 
 public class BallPane extends Pane {
+	// random radius
 	Random random = new Random();
 	public final double radius = random.nextInt(75)+1;
 	private double dx = 1, dy = 1; 
 	private double x = radius, y = radius;
-  
+	
 	private Circle circle = new Circle(x, y, radius);
 	private Timeline animation;
 
-  
+	private double trackX = 0.0;
+	private double trackY = 0.0;
+
 	//private double dx = random.nextInt(250)+1, dy = random.nextInt(150)+1;  
 	public BallPane() {
+		// random color
 		circle.setFill(Color.rgb((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256))); // Set ball color
 		getChildren().add(circle); // Place a ball into this pane
 
@@ -26,12 +30,22 @@ public class BallPane extends Pane {
 		new KeyFrame(Duration.millis(50), e -> moveBall()));
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.play(); // Start animation
-  }
+	}
 
+	/* new a circle to record the track */
 	public void newBall(){
+		
 		Circle ballObj = new Circle(x, y, radius);
-		//ballObj.setFill(Color.rgb((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256))); // Set ball color
+		ballObj.setFill(Color.web("rgba(255,255,255,0)"));
+		ballObj.setStroke(Color.rgb((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256))); // Set ball color
 		getChildren().add(ballObj); // Place a ball into this pane	
+		
+		if(trackX!=0.0 && trackY!=0.0){
+			Line line = new Line(trackX,trackY,x,y);
+			getChildren().add(line); // Place a track line into this pane	
+		}
+		trackX = x;
+		trackY = y;
 	}
 	public void play() {
 		animation.play();
